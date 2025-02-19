8876c929368d4743390258c68ee4b118b6516b4b
Jayden Smith <jayden.smith2@stu.ocsb.ca>
2025-02-19 04:01:08 +0000

    Added areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2025 Jayden Smith All rights reserved.
+// Created by: Jayden Smith
+// Date: February 18, 2025
+// This program calculates the area and the perimeter of a rectangle.
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has the dimensions" << std::endl;
+    std::cout << "9cm x 13cm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is: " << (9*13) << "cm^2" << std::endl;
+    std::cout << "Perimeter is: " << ((9*2) +(13*2)) << "cm^2" << std::endl;
+}
